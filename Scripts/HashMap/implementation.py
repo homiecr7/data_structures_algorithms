04c6b1714e1910a89b4f20ff98198db1d91b117f
homiecr7 <hammadazhar1997@gmail.com>
2023-05-18 23:17:23 +0500

    added comments

--- Scripts/HashMap/implementation.py
+++ Scripts/HashMap/implementation.py
@@ -1,11 +1,11 @@
 # implementing own hash tables
 
 class hash_table():
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, size): # constructor initialization
+        self.size = size # create an array size "size" and value None
         self.data = [None] * self.size
 
-    def __str__(self):
+    def __str__(self): # print the attributes of the object in the form of dictionary
         return str(self.__dict__)
 
     def _hash(self, key): #Our custom hash function
@@ -14,21 +14,21 @@ class hash_table():
             hash = (hash + ord(key[i])*i) % self.size #ord(key[i]) gives the unicode code point of the character key[i]
         return hash #The hash value obtained after applying the hash function to the key is returned
     
-    def set(self, key, value):
-        hash = self._hash(key)
-        if not self.data[hash]:
+    def set(self, key, value):  # sets the value in a dictionary
+        hash = self._hash(key) # get the hash for the location
+        if not self.data[hash]: # if location is not occupied
             self.data[hash] = [[key, value]]
-        else:
+        else: # if location is occupied
             self.data[hash].append([key, value])
     
-    def get(self, key):
-        hash = self._hash(key)
-        if self.data[hash]:
-            for i in self.data[hash]:
+    def get(self, key): # gets the value
+        hash = self._hash(key) # locates the hashed key
+        if self.data[hash]: # if the location is occupied
+            for i in self.data[hash]: # looping over each array to check the key
                 if key in i:
                     return i[1]
-            return "Key doesn't exist"
-        else:
+            return "Key doesn't exist" # if key doesnt exist
+        else: # if hash doesnt exist
             return "Key doesn't exist"
 
 new_hash = hash_table(2)
