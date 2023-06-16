fe7633a2e63c2bb8d937f047584b0b7d7164a61a
homiecr7 <hammadazhar1997@gmail.com>
2023-06-16 18:41:56 +0500

    added reverse function in linked list

--- Scripts/LinkedList/implementation.py
+++ Scripts/LinkedList/implementation.py
@@ -16,10 +16,11 @@ class LinkedList():
             print("Empty")
         else:
             current_node = self.head
-            while current_node!= None:
+            while current_node != None:
                 print(current_node.data, end= ' ')
                 current_node = current_node.next
         print()
+
     def __str__(self): # print the attributes of the object in the form of dictionary
         return str(self.__dict__)
 
@@ -29,10 +30,12 @@ class LinkedList():
             self.head = newNode
             self.tail = self.head
             self.length = 1
+            return
         else:
             self.tail.next = newNode
             self.tail = newNode
             self.length += 1
+            return
     
     def prepend(self, value):
         newNode = Node(value)
@@ -81,21 +84,40 @@ class LinkedList():
         currentNode.next = deletedNode.next
         del deletedNode
         self.length -= 1
+        return
     
     def reverse(self):
-        
-                    
+        if self.length <= 0 or self.length == 1:
+            print("not linked list present or only one node present")
+        else:
+            currentNode = self.head
+            nextNode = None
+            previousNode = None
+
+            while currentNode != None:
+                nextNode = currentNode.next
+                currentNode.next = previousNode
+                previousNode = currentNode
+                currentNode = nextNode
+            tempNode = self.head
+            self.head = self.tail
+            self.tail = tempNode
+            return
+
 myLinkedList = LinkedList()
-myLinkedList.append(5)
-myLinkedList.append(6)
-myLinkedList.append(8)
+myLinkedList.append(1)
+myLinkedList.append(2)
 myLinkedList.prepend(2)
 myLinkedList.prepend(7)
 myLinkedList.insert(2, 9)
+myLinkedList.print_list()
 myLinkedList.insert(28, 19)
 myLinkedList.print_list()
-myLinkedList.remove(3)
-myLinkedList.remove(1)
+# myLinkedList.print_list()
+# myLinkedList.remove(3)
+# myLinkedList.remove(1)
+# myLinkedList.print_list()
+myLinkedList.reverse()
 myLinkedList.print_list()
 
 
